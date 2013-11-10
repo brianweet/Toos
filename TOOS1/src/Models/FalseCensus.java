101d37da3d228b2e49dc1eab6a947cd5654afdb9
Wadie Assal <wadie@MacBook-Pro-van-Wadie.local>
2013-11-10 19:47:21 +0100

    Corrected comment

--- TOOS1/src/Models/FalseCensus.java
+++ TOOS1/src/Models/FalseCensus.java
@@ -2,7 +2,7 @@ package Models;
 
 public class FalseCensus extends Census {
 	
-	// return the total of voters that voted true
+	// return the total of voters that voted false
 	public int getFalseVoters() {
 		int falsevotes = 0;
 		
